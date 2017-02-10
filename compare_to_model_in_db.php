235135819270b17a34bc10a44a368d498501910f
Danail Koychev <danail.k.koychev@gmail.com>
2017-02-10 23:49:35 +0200

    change decision rule for acceptable similarity

--- compare_to_model_in_db.php
+++ compare_to_model_in_db.php
@@ -84,26 +84,26 @@ function parliament($current_data, $sigmas, $percentage, $hist){
       if(isset($model['bins']) && count($hist) != 0) {
     # MERGE BINS 50 ------------
           $metric_hist = bhatta($model['bins'], $hist);
-          //$metric_hist50 = bhatta(merge_bins_50($model['bins']),merge_bins_50($hist));
-
-          //echo print_r(merge_bins_50($hist));
-          //echo round($metric_hist, 2) . " " .
-              //round($metric_hist50, 2). " " . $username . "\n"; 
-
             
-          //echo round($metric_hold, 2) . "   " . round($metric_hist, 2) ."   ";
-          //echo "p+:" . round(($metric_hist*0.5 + 0.5*$metric_hold), 2) .  "  " . "p*:" . round(($metric_hist*$metric_hold), 2) . "   " . $username . "\n";
+          echo round($metric_hold, 2) . "   " . round($metric_hist, 2) ."   ";
+          echo "p+:" . round(($metric_hist*0.5 + 0.5*$metric_hold), 2) .  "  " . "p*:" . round(($metric_hist*$metric_hold), 2) . "   " . $username . "\n";
 
           $parliament_decision = $metric_hist*0.5 + 0.5*$metric_hold;
-          if($parliament_decision > 0.2){
-            array_push($within_limit, array($username, $parliament_decision));
-          }
+
+          
+          array_push($within_limit, array($username, $parliament_decision));
       }
     }
   }
   $conn = null;
   //return $within_limit; 
   usort($within_limit, "compare_similarity");
+  for($i = 1; $i < count($within_limit); $i++){
+      if($within_limit[0][1] - $within_limit[$i][1] > 0.02){
+          $within_limit = array_slice($within_limit, 0, $i );
+          break;
+      }
+  }
   return $within_limit;
 }
 
